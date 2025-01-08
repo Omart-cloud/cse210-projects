840922f79690d7afa1a615fba9dbe8b417c7c718
omart <omotosomarytaiwo@gmail.com>
2025-01-08 16:15:11 +0100

    Using_strings

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Sandbox Project.");
+        
+        Console.WriteLine("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        Console.WriteLine("What is your last name? ");
+        string last_name = Console.ReadLine();
+        
+        Console.Write($"Your name is {first_name} {last_name} {first_name}.");
+
+        
     }
 }
