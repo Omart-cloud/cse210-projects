634241982edbf6e26789dfb954b46d16db0a4249
omart <omotosomarytaiwo@gmail.com>
2025-01-10 08:00:10 +0100

    week01_exercise1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            // Generate a random magic number between 1 and 100
+            Random randomNumber = new Random();
+            int magicNumber = randomNumber.Next(1, 101);
+
+            int guess = -1;
+            int guessCount = 5;
+
+            Console.WriteLine("A magic number between 1 and 100 has been generated. Try to guess it!");
+
+            // Loop until the guess matches the magic number
+            while (guess != magicNumber)
+            {
+                Console.Write("What is your guess?: ");
+                guess = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if (magicNumber > guess)
+                {
+                    Console.WriteLine("Higher!");
+                }
+                else if (magicNumber < guess)
+                {
+                    Console.WriteLine("Lower!");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed right!");
+                }
+            }
+
+            // Inform the user of their guess count
+            Console.WriteLine($"It took you {guessCount} guesses to find the magic number!");
+
+            // Ask if they want to play again
+            Console.Write("Do you want to play again? (yes/no): ");
+            string response = Console.ReadLine().ToLower();
+
+            if (response != "yes")
+            {
+                playAgain = false; // Exit the loop if the user doesn't want to play again
+            }
+        }
     }
 }
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int num;
+
+        while (true)
+        {
+            Console.Write("Enter a number (enter 0 to stop): ");
+            num = int.Parse(Console.ReadLine());
+
+            if (num == 0)
+            {
+                break;
+            }
+
+            numbers.Add(num);
+        }
+
+        int total = 0;
+        foreach (int n in numbers)
+        {
+            total += n;
+        }
+
+        double average = numbers.Count > 0 ? (double)total / numbers.Count : 0;
+
+        int maximum = numbers.Count > 0 ? numbers[0] : 0;
+        foreach (int n in numbers)
+        {
+            if (n > maximum)
+            {
+                maximum = n;
+            }
+        }
+
+        Console.WriteLine($"The total of the numbers is: {total}");
+        Console.WriteLine($"The average of the numbers is: {average}");
+        Console.WriteLine($"The largest number is: {maximum}");
     }
 }
